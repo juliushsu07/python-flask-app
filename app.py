95562a41e35f4fe392d7f9f2f2c895a76f8172fe
Julius Hsu <juliushsu07@gmail.com>
2022-11-26 01:00:42 -0500

    Seperate out views into their own files.

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Hello World!"
-
-@app.route("/about")
-def about():
-    return "<h1 style='color: red'>About !!</h1>"
+from app import app
 
 if __name__ == "__main__":
     app.run()
